f2e21e4703c657561d36905731942aacb339bf42
Hendrik Hilleckes <hendrikhilleckes@192.168.0.13>
2016-09-13 17:39:35 +0200

    Initial commit

---
+++ restml/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RESTml',
+      version='0.1',
+      description='Create an image classifier and a RESTful service to classify images',
+      url='http://github.com/hhllcks/restml',
+      author='hhllcks',
+      author_email='hhllcks@gmail.com',
+      license='GPLv3',
+      packages=['restml'],
+      zip_safe=False)
